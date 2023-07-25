ad11c2fa05bb9044fbdf073e2d7154a068327512
노희찬 <hcr@Exertory.local>
2023-07-25 11:50:43 +0900

    git remote configuration

---
+++ example.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import altair as alt
+import numpy as np
+import pandas as pd
+
+st.header("Demo")
+
+st.write("1번")
+st.write("2번")
+
+df = pd.DataFrame({"first": [1, 2, 3, 4], "second": ["가", "나", "다", "라"]})
+
+st.write(df)
+
+st.write("공백")
+
+st.write("데이터프레임", df, "데이터프레임")
+
+df2 = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+c = (
+    alt.Chart(df2)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.write(c)
